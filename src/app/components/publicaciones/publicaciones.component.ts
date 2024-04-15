8e20085fdacfcf1fd50657fc775e066051b6f6f9
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-15 18:40:44 -0500

    feat: :sparkles: add changes

--- src/app/components/publicaciones/publicaciones.component.ts
+++ src/app/components/publicaciones/publicaciones.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PublicacionesService } from '../../services/publicaciones.service';
+import { PublicacionesInterface } from '../../interfaces/publicaciones.interface';
 
 @Component({
   selector: 'app-publicaciones',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './publicaciones.component.html',
   styleUrl: './publicaciones.component.css'
 })
-export class PublicacionesComponent {
+export class PublicacionesComponent implements OnInit{
+  
+  publicacionList: PublicacionesInterface[] = [];
+
+  constructor(private publicacionesService: PublicacionesService){}
+
+  ngOnInit(): void {
+    this.getPublicaciones();
+  }
+
+  getPublicaciones(){
+    this.publicacionesService.getPublicaciones().subscribe({
+      next: (result: any) => {
+        this.publicacionList = result;
+      },
+      error: (error: Error)=>{
+        console.log(error);
+      }
+    });
+  }
 
 }
---
+++ src/app/interfaces/publicaciones.interface.ts
@@ -0,0 +1,6 @@
+export interface PublicacionesInterface {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
