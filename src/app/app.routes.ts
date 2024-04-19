5172ba1f5ac4fbcda6b7d12a6fa01471e4ccae48
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-19 18:22:59 -0500

    feat: :sparkles: Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { NosotrosPageComponent } from './page/nosotros-page/nosotros-page.component';
+import { ContactenosPageComponent } from './page/contactenos-page/contactenos-page.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
+import { PublicacionesPageComponent } from './page/publicaciones-page/publicaciones-page.component';
 
 export const routes: Routes = [
     {
         path:'home',
         component: HomePageComponent
+    },
+    {
+        path:'nosotros',
+        component: NosotrosPageComponent
+    },
+    {
+        path:'nosotros/contactenos',
+        component: ContactenosPageComponent
+    },
+    {
+        path:'publicaciones',
+        component:PublicacionesPageComponent
     }
 ];
